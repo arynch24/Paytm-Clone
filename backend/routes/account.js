94eb89905c4a8abae887d47acc03efc0d585fe43
Aryan <arynch24@gmail.com>
2025-04-18 19:25:24 +0530

    Refactor account routes and add transaction functionality

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,17 +1,71 @@
 const express = require('express');
 const { authMiddleware } = require('../middleware');
 const { Accounts } = require('../db');
+const { default: mongoose } = require('mongoose');
 const router = express.Router();
 
 
-router.get('/balance',authMiddleware,async (req,res)=>{
+router.get('/balance', authMiddleware, async (req, res) => {
     const userAccount = await Accounts.findOne({
-        userId:req.userId
+        userId: req.userId
     })
 
     res.statq(200).json({
-        balance:userAccount.balance
+        balance: userAccount.balance
     })
+})
+
+
+//we need to ensure that a complete transaction should happen without any failure
+//If something crashes in between the transaction should be rolled back
+//samajh gaye?
+
+//session also prevents from happening another transaction from the same user if any transaction is ongoing
+router.post('/transfer', authMiddleware, async (req, res) => {
+    //for that we will use session provided by  mongoose
+
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+
+    const { to, amount } = req.body;
+
+    const senderAccount = await Accounts.findOne({ userId: req.userId }).session(session);
+    if (senderAccount.balance < amount) {
+        res.status(400).json({
+            message: "Insuficient Balance"
+        })
+    }
+
+    const reciversAccount = await Accounts.findOne({ userId: to }).session(session);
+    if (!reciversAccount) {
+        res.status(400).json({
+            message: "Insuficient Balance"
+        })
+    }
+
+    //decrease balance of senders account
+    await Accounts.updateOne({ userId: req.userId }, {
+        '$inc': {
+            balance: -amount
+        }
+    }).session(session)
+
+    //increase balance of recivers
+    await Accounts.updateOne({ userId: to }, {
+        '$inc': {
+            balance: amount
+        }
+    }).session(session)
+
+    // Commit the transaction
+    await session.commitTransaction();
+
+    res.json({
+        message: "Transfer successful"
+    })
+
+
 })
 
 module.exports = router;
