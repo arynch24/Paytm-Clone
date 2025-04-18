547dbb220886c2f082bde696b5d8a162cf28647e
Aryan <arynch24@gmail.com>
2025-04-19 02:01:29 +0530

    Refactor user routes and update Users and SendMoney components

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Users = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+
+        const fetchUsers = async () => {
+
+            try {
+                const response = await axios.get('http://localhost:3000/api/v1/user/bulk',);
+                console.log(`Users: ${response.data}`);
+                setUsers(response.data.users);
+            }
+            catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+
+        fetchUsers();
+    }
+        , []);
+
+    const navigate = useNavigate();
+
+    return (
+        <div className='m-8'>
+            <h3 className='text-xl font-bold'>Users</h3>
+            <input type="text" className=' w-full mb-4 border-2 border-gray-300 rounded-md p-2 mt-4' placeholder='Search users...' />
+            <div className='flex flex-col'>
+                {users.map((user) => (
+                    <div key={user._id} className='flex justify-between items-center border-b-1 border-gray-300 p-2'>
+                        <div className='flex gap-4 justify-center items-center'>
+                            <div className='h-8 w-8 rounded-full bg-gray-300 p-5 text-xl font-semibold flex justify-center items-center'>{user.firstName[0]}</div>
+                            <h4 className='text-lg font-semibold'>{user.firstName} {user.lastName}</h4>
+                        </div>
+                        <div className='flex gap-4'>
+                            <button className='bg-black hover:bg-zinc-900 text-white font-bold py-2 px-4 rounded' onClick={
+                                () => {
+                                    navigate('/send');
+                                }
+                            }>Send Money</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Users
--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SendMoney = () => {
   return (
     <div>
-      
+      <h3 className='text-xl font-bold'>Send Money</h3>
     </div>
   )
 }
