92550a401cf2b8db1125c2242d08d463e1b25e13
Aryan <arynch24@gmail.com>
2025-04-18 20:33:54 +0530

    Add React and Vite configuration files and initial code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/signup"} element={<SignUp />}></Route>
+        <Route path={"/signin"} element={<SignIn />}></Route>
+        <Route path={"/dashboard"} element={<Dashboard />}></Route>
+        <Route path={"/send"} element={<SendMoney />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
